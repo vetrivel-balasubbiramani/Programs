13d0a8d57eed361bbb9ab9ac410fe1e9b09ae3d7
vetrivel-balasubbiramani <142219572+vetrivel-balasubbiramani@users.noreply.github.com>
2023-09-27 12:06:19 +0530

    Updated project

--- shopping_app/script/details.js
+++ shopping_app/script/details.js
@@ -1,52 +1,38 @@
-
-
 function ProductViewModel() {
   const self = this;
-
   self.cartCount = ko.observable(0);
-
   self.selectedProduct = ko.observable();
-
-  self.calculateCartCount = function() {
-    const cartData = sessionStorage.getItem('cart');
-    const cart = cartData ? JSON.parse(cartData) : {};
-
-    let totalCount = 0;
-
-    for (const productName in cart) {
-      totalCount += cart[productName].quantity;
-    }
-
-    return totalCount;
+  self.getCartData = function () {
+    const cartData = sessionStorage.getItem("cart");
+    return cartData ? JSON.parse(cartData) : {};
   };
-
-  self.addToCart = function() {
+  self.calculateCartCount = function () {
+    const cart = self.getCartData();
+    return Object.values(cart).reduce((totalCount, item) => totalCount + item.quantity, 0);;
+  };
+  self.addToCart = function () {
     const product = self.selectedProduct();
     const quantity = parseInt(product.quantity, 10);
 
-    if (!isNaN(quantity) && quantity > 0) { <script data-main="index" src="require.js"> </script>
-      const cartData = sessionStorage.getItem('cart');
-      const cart = cartData ? JSON.parse(cartData) : {};
-
+    if (!isNaN(quantity) && quantity > 0) {
+      const cart = self.getCartData();
       if (cart.hasOwnProperty(product.name)) {
         cart[product.name].quantity += quantity;
       } else {
         cart[product.name] = {
           quantity: quantity,
-          price: product.price
+          price: product.price,
         };
       }
-
-      sessionStorage.setItem('cart', JSON.stringify(cart));
+      sessionStorage.setItem("cart", JSON.stringify(cart));
       self.cartCount(self.calculateCartCount());
       alert(`${quantity} ${product.name}(s) added to cart!`);
     } else {
-      alert('Please select a valid quantity.');
+      alert("Please select a valid quantity.");
     }
   };
-
   ko.computed(function () {
-    const selectedProductJSON = sessionStorage.getItem('selectedProduct');
+    const selectedProductJSON = sessionStorage.getItem("selectedProduct");
     if (selectedProductJSON) {
       const parsedProduct = JSON.parse(selectedProductJSON);
       self.selectedProduct(parsedProduct);
@@ -54,7 +40,5 @@ function ProductViewModel() {
     }
   });
 }
-
 const viewModel = new ProductViewModel();
 ko.applyBindings(viewModel);
-
